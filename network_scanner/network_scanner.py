7e0be177a75e300cc3ccf7490f120e6da8f7c88b
ninabarzh <30133030+ninabarzh@users.noreply.github.com>
2022-01-14 04:37:11 +0100

    Initial commit

--- network_scanner/network_scanner.py
+++ network_scanner/network_scanner.py
@@ -1,8 +1,42 @@
 #! /usr/bin/env python
 
 import scapy.all as scapy
+import optparse
+
+
+def get_args():
+    parser = optparse.OptionParser()
+    parser.add_option("-t", "--target", dest="ip", help="IP address")
+    (values, args) = parser.parse_args()
+    if not values.ip:
+        parser.error("[-] Please specify a target IP address, use --help for more information")
+    return values
+
 
 def scan(ip):
-    scapy.arping(ip)
+    # Create ARP broadcast request
+    arp_request = scapy.ARP(pdst=ip)
+    broadcast = scapy.Ether(dst="ff:ff:ff:ff:ff:ff")
+    arp_request_broadcast = broadcast/arp_request
+    # Send and receive
+    answers = scapy.srp(arp_request_broadcast, timeout=1, verbose=False)[0]
+    # Parse responses
+    clients = []
+    for answer in answers:
+        client = {"ip": answer[1].psrc, "mac": answer[1].hwsrc}
+        clients.append(client)
+    return clients
 
-scan("192.168.122.1")
+
+def print_results(results):
+    print("IP address\t\tMAC Address")
+    print("-----------------------------------------")
+    for result in results:
+        print(result["ip"] + "\t\t" + result["mac"])
+    print("-----------------------------------------")
+
+
+# scan for example 192.168.122.1/24
+options = get_args()
+scan_results = scan(options.ip)
+print_results(scan_results)
